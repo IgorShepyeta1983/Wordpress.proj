939dada07a064bc7ee69eef0fa62c87073168d58
IgorShepyeta1983 <139997075+IgorShepyeta1983@users.noreply.github.com>
2023-07-25 19:04:21 -0400

    Wordpress_proj

---
+++ IGOR/main.tf
@@ -0,0 +1,184 @@
+provider "aws" {
+  region     = "us-east-1"   # Replace this with your desired AWS region
+
+}
+resource "aws_vpc" "wordpress-vpc" {
+  cidr_block = "10.0.0.0/16"  # Replace this with the desired CIDR block for your VPC
+  tags = {
+    Name = "wordpress-vpc"
+  }
+}
+resource "aws_internet_gateway" "wordpress_igw" {
+  vpc_id = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  
+  tags = {
+    Name = "wordpress_igw"
+  }
+}
+# subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  cidr_block              = "10.0.1.0/24"      # Replace this with the desired CIDR block for your subnet
+  availability_zone       = "us-east-1a"       # Replace this with the desired availability zone
+  map_public_ip_on_launch = true               # Set this to true if you want to auto-assign public IP addresses to instances in this subnet.
+
+  tags = {
+    Name = "public_subnet_1"
+  }
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  cidr_block              = "10.0.2.0/24"      # Replace this with the desired CIDR block for your subnet
+  availability_zone       = "us-east-1a"       # Replace this with the desired availability zone
+  map_public_ip_on_launch = true               # Set this to true if you want to auto-assign public IP addresses to instances in this subnet.
+
+  tags = {
+    Name = "public_subnet_2"
+  }
+}
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id                  = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  cidr_block              = "10.0.3.0/24"      # Replace this with the desired CIDR block for your subnet
+  availability_zone       = "us-east-1a"       # Replace this with the desired availability zone
+  map_public_ip_on_launch = true               # Set this to true if you want to auto-assign public IP addresses to instances in this subnet.
+
+  tags = {
+    Name = "public_subnet_3"
+  }
+}
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  cidr_block              = "10.0.4.0/24"      # Replace this with the desired CIDR block for your subnet
+  availability_zone       = "us-east-1a"       # Replace this with the desired availability zone
+
+  tags = {
+    Name = "private_subnet_1"
+  }
+}
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id                  = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  cidr_block              = "10.0.5.0/24"      # Replace this with the desired CIDR block for your subnet
+  availability_zone       = "us-east-1a"       # Replace this with the desired availability zone
+
+  tags = {
+    Name = "private_subnet_2"
+  }
+}
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id                  = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+  cidr_block              = "10.0.6.0/24"      # Replace this with the desired CIDR block for your subnet
+  availability_zone       = "us-east-1a"       # Replace this with the desired availability zone
+
+  tags = {
+    Name = "private_subnet_3"
+  }
+}
+resource "aws_route_table" "wordpess-rt" {
+  vpc_id = aws_vpc.wordpress-vpc.id  # Replace `aws_vpc.example` with the reference to your VPC resource.
+
+  tags = {
+    Name = "wordpess-rt"
+  }
+}
+
+
+resource "aws_route_table" "wordpess-rt_nat" {
+  vpc_id = aws_vpc.wordpress-vpc.id
+
+  tags = {
+    Name = "wordpess-rt_nat"
+  }
+}
+
+#route tables
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.wordpess-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.wordpress_igw.id  # Replace `aws_internet_gateway.example` with the reference to your Internet Gateway resource.
+}
+#route tables to public subnets
+resource "aws_route_table_association" "public_subnet1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.wordpess-rt.id
+}
+resource "aws_route_table_association" "public_subnet2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.wordpess-rt.id
+}
+resource "aws_route_table_association" "public_subnet3_association" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.wordpess-rt.id
+}
+# NAT gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id  # Replace `aws_subnet.public_subnet` with the reference to your public subnet
+}
+resource "aws_route" "nat_gateway_route" {
+  route_table_id         = aws_route_table.wordpess-rt_nat.id  
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+resource "aws_route_table_association" "private_subnet1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.wordpess-rt_nat.id
+}
+
+
+#security group
+resource "aws_security_group" "wordpress-sg" {
+  name        = "wordpress-sg"
+  description = "wordpress-sg security group"
+
+  # Inbound rules (allow incoming traffic)
+  ingress {
+    from_port   = var.ingress_ports[0]  # Change this to the desired port
+    to_port     = var.ingress_ports[0] # Change this to the desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this to limit the IP range if needed
+  }
+  ingress {
+    from_port   = var.ingress_ports[1] # Change this to the desired port
+    to_port     = var.ingress_ports[1] # Change this to the desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this to limit the IP range if needed
+  }
+  ingress {
+    from_port   = var.ingress_ports[2]  # Change this to the desired port
+    to_port     = var.ingress_ports[2] # Change this to the desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this to limit the IP range if needed
+  }
+  # Add more inbound rules if required
+
+  # Outbound rules (allow outgoing traffic)
+  egress {
+    from_port   = var.ingress_ports[0]  # Change this to the desired port
+    to_port     = var.ingress_ports[0]# Change this to the desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this to limit the IP range if needed
+  }
+  egress {
+    from_port   = var.ingress_ports[1]  # Change this to the desired port
+    to_port     = var.ingress_ports[1]  # Change this to the desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this to limit the IP range if needed
+  }
+  egress {
+    from_port   = var.ingress_ports[2]  # Change this to the desired port
+    to_port     = var.ingress_ports[2]  # Change this to the desired port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Change this to limit the IP range if needed
+  }
+  tags = {
+    Name = "wordpess-sg"
+  }
+  # Add more outbound rules if required
+}
+variable "ingress_ports" {
+  type = list(string)
+  default = ["80","443","22"]  
+}
